b24a214a3d8d0c3c2c7dccaeae3ec54bc68e7d4f
BahacanKaratas <bahacankaratas@gmail.com>
2021-10-04 14:40:46 +0300

    Gradle Build Success and localhost456 can be opened now. Fixed the previous error 2.

--- app/src/main/java/Hw1Final481/App.java
+++ app/src/main/java/Hw1Final481/App.java
@@ -23,7 +23,7 @@ public class App {
       public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
-        get("/", (req, res) -> "Hello, World");
+        get("/", (req, res) -> "Hello, World 2");
 
         post("/compute", (req, res) -> {
           //System.out.println(req.queryParams("input1"));
